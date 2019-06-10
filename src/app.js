b08d83bc4dc1ce03473b4e9cbe65c605660f332a
renyakun <13843076014@163.com>
2019-06-10 19:46:04 +0800

    ipad

--- src/app.js
+++ src/app.js
@@ -1,47 +1,67 @@
 // app.js
-import { wxLogin, checkToken, refreshToken} from './utils/api.js'
+import { wxLogin, checkToken, refreshToken } from './utils/api.js'
 import * as store from './utils/store.js'
 App({
-  onLaunch: function() {
-    wx.qy.checkSession({
-      success: function () {
-        console.log('success')
-        checkToken().then(res => {
-          if (res.data.code == 0 & res.data.data == false) {
-            refreshToken().then(res => {
-              console.log(res, 'refresh')
-            })
-          }
-          console.log(res, '===========')
-        })
-        // session_key 未过期，并且在本生命周期一直有效
-      },
-      fail: function () {
-        console.log('fail----')
-        // session_key 已经失效，需要重新执行登录流程
-        wx.qy.login({
-          success: res => {
-            // 发送 res.code 到后台换取 openId, sessionKey, unionId
-            console.log({
-              code: res.code,
-              brand: 'CAR',
-              agentId: '1000007',
-              type: 'binding'
+  onLaunch: function () {
+    // 获取系统状态栏信息
+    wx.getSystemInfo({
+      success: e => {
+        console.log(e, '-----------------')
 
-            }, '===============')
-            wxLogin({
-              code: res.code,
-              brand: 'CAR',
-              agentId: '1000007',
-              type: 'binding'
-
-            }).then(res => {
-              store.set('token', res.token)
-            })
-          }
-        })
+        this.globalData.StatusBar = e.statusBarHeight
+        let custom = wx.getMenuButtonBoundingClientRect()
+        console.log(custom, '=============')
+        this.globalData.Custom = custom
+        if (e.environment == 'wxwork') {
+          this.globalData.env = e.environment
+          this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight + 45
+        } else {
+          this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight
+        }
       }
     })
+    if (this.globalData.env == 'wxwork') {
+      wx.qy.checkSession({
+        success: function () {
+          console.log('success')
+          checkToken().then(res => {
+            if (res.data.code == 0 & res.data.data == false) {
+              refreshToken().then(res => {
+                console.log(res, 'refresh')
+              })
+            }
+            console.log(res, '===========')
+          })
+          // session_key 未过期，并且在本生命周期一直有效
+        },
+        fail: function () {
+          console.log('fail----')
+          // session_key 已经失效，需要重新执行登录流程
+          wx.qy.login({
+            success: res => {
+              // 发送 res.code 到后台换取 openId, sessionKey, unionId
+              console.log({
+                code: res.code,
+                brand: 'CAR',
+                agentId: '1000007',
+                type: 'binding'
+
+              }, '===============')
+              wxLogin({
+                code: res.code,
+                brand: 'CAR',
+                agentId: '1000007',
+                type: 'binding'
+
+              }).then(res => {
+                store.set('token', res.token)
+              })
+            }
+          })
+        }
+      })
+    }
+
 
     // 获取用户信息
     wx.getSetting({
@@ -63,22 +83,7 @@ App({
         }
       }
     })
-    // 获取系统状态栏信息
-    wx.getSystemInfo({
-      success: e => {
-        console.log(e, '-----------------')
 
-        this.globalData.StatusBar = e.statusBarHeight
-        let custom = wx.getMenuButtonBoundingClientRect()
-        console.log(custom, '=============')
-        this.globalData.Custom = custom
-        if (e.environment == 'wxwork') {
-          this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight + 45
-        } else {
-          this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight
-        }
-      }
-    })
   },
   login() {
     // 登录
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -12,7 +12,7 @@ Page({
     modalName: '',
 
     userInfo: {},
-    users: [],
+    users: [{ "externalUserId": "wmdb3IDgAA-Vztf7iXpOlBZ2qoCaejOA", "bind": true, "cdbId": "1234", "bindingDate": "2019-05-23 11:34:51", "name": "test.external", "avatar": "http://p.qlogo.cn/bizmail/IcsdgagqefergqerhewSdage/0" }, { "externalUserId": "wmdb3IDgAA-adfadfdpOldafDaFaejAD", "bind": false, "name": "test.external", "avatar": "http://p.qlogo.cn/bizmail/Adfad3efergqerhewSdage/0" }],
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -25,8 +25,7 @@ Page({
         users: res.data.data
 
       })
-      store.set('myuers', res.data.data)
-      console.log(res, 'select----------------')
+
     })
   },
   selectExternal() {
@@ -35,57 +34,53 @@ Page({
       filterType: 0, // 0表示展示全部外部联系人列表，1表示仅展示未曾选择过的外部联系人。默认值为0；除了0与1，其他值非法。
       success: function (res) {
         var userIds = res.userIds// 返回此次选择的外部联系人userId列表，数组类型
-       
+
         self.getExternals(userIds)
       }
     })
   },
-  onShow: function () {
-    console.log(this.data.users, '======')
-  },
   onLoad: function () {
-    // let self = this
-    // let userIds =[]
-    // wx.qy.getCurExternalContact({
-    //   success: function (res) {
-    //     var userId = res.userId //返回当前外部联系人userId
-    //     userIds.push(userId)
-    //     self.getExternals(userIds)
-    //   }
-    // })
-    if (store.get('myusers')) {
-      this.setData({
-        users: store.get('myusers')
-      })
-      console.log(this.data.users, '======')
-    }
+    console.log(this.data.users, '======')
+    let self = this
+    let userIds = []
+    wx.qy.getCurExternalContact({
+      success: function (res) {
 
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+        var userId = res.userId //返回当前外部联系人userId
+        if (userId) {
+          userIds.push(userId)
+          self.getExternals(userIds)
+
         }
-      })
-    }
+
+      },
+      fail: function () {
+
+      }
+    })
+
+  },
+  onShow: function () {
+    let self = this
+    let userIds = []
+    wx.qy.getCurExternalContact({
+      success: function (res) {
+
+        var userId = res.userId //返回当前外部联系人userId
+        if (userId) {
+          userIds.push(userId)
+          self.getExternals(userIds)
+
+        }
+
+      },
+      fail: function () {
+
+      }
+    })
+
+
+
   },
   showModal(e) {
     console.log('test---------')
