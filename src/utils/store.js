d7a2e59414bf7267303d9fd7eb6c8e3463df3d2a
renyakun <13843076014@163.com>
2019-06-18 20:03:26 +0800

    dfad

--- src/utils/store.js
+++
@@ -1,6 +0,0 @@
-import { appid } from './config.js'
-export const get = key => wx.getStorageSync(`${appid}-${key}`);
-export const set = (key, value) => {
-  wx.setStorageSync(`${appid}-${key}`, value);
-};
-
